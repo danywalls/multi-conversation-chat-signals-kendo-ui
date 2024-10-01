3d7fc1c27ddcc298c617e83bb025fcbff98d338e
Dany Paredes <danywalls4@gmail.com>
2024-10-01 09:44:10 +0200

    feat:  add type and mock conversations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  ChatConversation,
+  mockConversations,
+} from '../entities/chat-conversation';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'conversation-chat';
+  currentConversation = signal<ChatConversation | null>(null);
+
+  conversations = signal<ChatConversation[]>(mockConversations);
 }
---
+++ src/entities/chat-conversation.ts
@@ -0,0 +1,33 @@
+import { Message } from '@progress/kendo-angular-conversational-ui';
+
+export type ChatConversation = {
+  id: number;
+  name: string;
+  pinned: boolean;
+  messages: Array<Message>;
+};
+
+export const mockConversations = [
+  {
+    id: 1,
+    name: 'NBA Chat',
+    pinned: true,
+    messages: [
+      {
+        text: 'Let’s chat about the NBA!',
+        author: {
+          name: 'Dany ',
+          id: 1,
+        },
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Kendo UI Chat',
+    pinned: false,
+    messages: [
+      { text: 'Want to learn Kendo UI?', author: { name: 'Mandy AI' } },
+    ],
+  },
+];
